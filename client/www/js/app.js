2683509f4e64f4a7bf96aa4a5d7567bf378f98bc
Lana-chan <erin.pinheiro@usp.br>
2016-12-21 19:37:00 -0200

    tweaks gps e leaflet

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -4,23 +4,24 @@
  */
 
 // Initialize your app
-var Frm7 = new Framework7();
-
+var Frm7 = new Framework7({
+   //swipePanel: 'left'
+});
 // Export selectors engine
 var $$ = Dom7;
 
 // Add view
 var mainView = Frm7.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    //dynamicNavbar: true
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
 Frm7.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
+    /*$$('.create-page').on('click', function () {
         createContentPage();
-    });
+    });*/
 });
 
 var app = {
@@ -42,6 +43,10 @@ var app = {
   // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function() {
     //app.receivedEvent('deviceready');
+    if (cordova.platformId == 'android') {
+      StatusBar.backgroundColorByHexString("#1C81CE");
+      //StatusBar.backgroundColorByHexString("#1C81CE");
+    }
   },
 
   onDocumentReady: function() {
@@ -62,21 +67,36 @@ var app = {
 
 var map = {
   object: null,
+  dot: null,
 
   init: function() {
     map.object = new L.Map('map');
 
   	url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  	opt = {attribution: "<a href='https://www.openstreetmap.org/'>OSM</a>"}
+  	opt = {attribution: "<a href='https://www.openstreetmap.org/'>OSM</a>"};
   	var layer = new L.TileLayer(url, opt);
 
   	map.object.addLayer(layer);
+    map.dot = L.marker([0, 0]).addTo(map.object);
     // geopos de são paulo
-  	map.object.setView(new L.LatLng(-23.5, -46.6),9);
+  	map.object.setView(new L.LatLng(-23.5, -46.6),50);
+
+    map.centerOnGPS();
   },
 
+  // finds gps, sets marker and pans camera to it
   centerOnGPS: function() {
-    map.object.locate({setView:true});
+    map.object.locate({
+      enableHighAccuracy:true,
+      setView:true
+    });
+    map.object.on('locationfound', map.updateDot);
+  },
+
+  // callback function to set marker
+  updateDot: function(e) {
+    map.dot.setLatLng(e.latlng)
+    map.dot.update();
   }
 };
 
