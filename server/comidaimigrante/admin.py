44697af01f230840ceb243cf5bb0a44ce3640dc2
Lana-chan <erin.pinheiro@usp.br>
2017-01-12 19:26:28 -0200

    conserta modelos e formulários textfield

--- server/comidaimigrante/admin.py
+++ server/comidaimigrante/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from comidaimigrante.models import Origem, Comida, Flag, Restaurante, Horario
+from comidaimigrante.models import Origem, Comida, Flag, Restaurante, Horario, Cidade
 # Register your models here.
 
-admin.site.register(Restaurante)
+class HorariosInline(admin.TabularInline):
+    model = Horario
+
+class RestauranteAdmin(admin.ModelAdmin):
+    inlines = [HorariosInline]
+
+admin.site.register(Restaurante, RestauranteAdmin)
 admin.site.register(Origem)
 admin.site.register(Comida)
 admin.site.register(Flag)
-admin.site.register(Horario)
+admin.site.register(Cidade)
--- server/comidaimigrante/models.py
+++ server/comidaimigrante/models.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext as _
 from django.db import models
+from django.contrib.admin import widgets
 
 # Create your models here.
 
@@ -13,30 +14,54 @@ WEEKDAYS = [
   (7, _("Domingo")),
 ]
 
+class StringField(models.TextField):
+    def formfield(self, **kwargs):
+        kwargs['widget'] = widgets.AdminTextInputWidget
+        return super(StringField, self).formfield(**kwargs)
+
+class Cidade(models.Model):
+    cidade = StringField()
+    estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.cidade + ' - ' + self.estado
+
 class Origem(models.Model):
-    nome = models.TextField(primary_key=True)
+    nome = StringField(primary_key=True)
     bandeira = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+
 class Comida(models.Model):
-    tag = models.TextField(primary_key=True)
+    tag = StringField(primary_key=True)
+
+    def __str__(self):
+        return self.tag
 
 class Flag(models.Model):
-    flag = models.TextField(primary_key=True)
+    flag = StringField(primary_key=True)
+
+    def __str__(self):
+        return self.flag
 
 class Restaurante(models.Model):
-    nome = models.TextField()
-    endereco = models.TextField()
-    cidade = models.TextField()
+    nome = StringField()
+    endereco = StringField()
+    cidade = models.ManyToManyField(Cidade)
     sinopse = models.TextField()
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     long = models.DecimalField(max_digits=9, decimal_places=6)
     telefone = models.CharField(max_length=11)
     origem = models.ForeignKey(Origem)
-    foto = models.ImageField(upload_to='fotos')
-    link = models.URLField()
+    foto = models.ImageField(blank=True, upload_to='fotos')
+    link = models.URLField(blank=True)
     preco = models.IntegerField()
     comida = models.ManyToManyField(Comida)
     flags = models.ManyToManyField(Flag)
+
+    def __str__(self):
+        return self.nome
 
 class Horario(models.Model):
     restaurante = models.ForeignKey(Restaurante)
