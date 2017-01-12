c1354d2c6bc086d99458d47392f7b33a8d7ba758
Lana-chan <erin.pinheiro@usp.br>
2017-01-12 17:58:40 -0200

    registra tabelas no admin

--- server/comidaimigrante/admin.py
+++ server/comidaimigrante/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from comidaimigrante.models import Origem, Comida, Flag, Restaurante, Horario
 # Register your models here.
+
+admin.site.register(Restaurante)
+admin.site.register(Origem)
+admin.site.register(Comida)
+admin.site.register(Flag)
+admin.site.register(Horario)
