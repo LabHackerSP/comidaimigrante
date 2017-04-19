740962633ab87475b02d1a8828f017f3f7d4142b
Lana-chan <erin.pinheiro@usp.br>
2017-04-19 11:44:42 -0300

    code scaffolding pro visitaço

---
+++ client/www/js/visitaco.js
@@ -0,0 +1,31 @@
+Frm7.onPageInit('visitaco', function(page) {
+  //init
+});
+
+var visitaco = {
+  // send add to event and check response
+  join: function() {
+
+  },
+
+  // send remove to event and check response
+  part: function() {
+
+  },
+
+  // check response and call update if 204
+  check: function() {
+
+  },
+
+  // updates object and then page
+  // status is 'add' or 'remove'
+  update: function(status) {
+
+  },
+
+  // returns true or false whether current logged user is in event
+  userInEvent: function() {
+
+  },
+}
