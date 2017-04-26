a668fe0755d679b35011fc50d1b0678a2c8864c2
Lana-chan <erin.pinheiro@usp.br>
2017-04-26 10:47:30 -0300

    url regex exata

--- server/comidaimigrante/urls.py
+++ server/comidaimigrante/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^accounts/profile', views.profile, name='profile'),
     url(r'^api/meta', views.meta, name='meta'),
-    url(r'^api/forms', views.forms, name='forms'),
-    url(r'^api/formsvisitaco', views.formsvisitaco, name='formsvisitaco'),
+    url(r'^api/forms$', views.forms, name='forms'),
+    url(r'^api/formsvisitaco$', views.formsvisitaco, name='formsvisitaco'),
     url(r'^api/visitar/(?P<evento>[0-9]+)/(?P<choice>[a-z]+)', views.visit, name='visit'),
 ]
